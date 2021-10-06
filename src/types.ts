388724c4dade0f323a0a80feb31fff5baf82cf19
Tyler Auer <Tyler.Auer@gmail.com>
2021-10-06 13:40:54 -0600

    Add types for generalizing simulation

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,21 @@ export type RandType = {
   cycle: number;
   running: boolean;
 };
+
+enum OddsTypes {
+  'dots',
+  'squares',
+}
+
+export type Odds = {
+  numerator?: number;
+  denominator: number;
+  type: keyof typeof OddsTypes;
+};
+
+export type Simulation = {
+  title: string;
+  description: string;
+  OddsList: Odds[];
+  initialClockSpeedInMs?: number;
+};
